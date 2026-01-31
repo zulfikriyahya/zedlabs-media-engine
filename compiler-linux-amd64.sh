2a5995337be3a63bda668f6e743c132da1d56fc3
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2026-01-31 23:57:10 +0700

    Add: Compiler file

--- compiler-linux-amd64.sh
+++ compiler-linux-amd64.sh
@@ -1,4 +1,4 @@
-sudo apt-get install python3-venv libtiff5 libxcb-cursor0
+sudo apt-get install python3-venv libtiff6 libxcb-cursor0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-shape0 -y
 python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
