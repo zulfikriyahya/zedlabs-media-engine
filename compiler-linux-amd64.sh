c38ab520771c24f27a38c8c0054d17b25eb84a79
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2026-01-31 23:53:01 +0700

    Add: Compiler file

--- compiler-linux-amd64.sh
+++ compiler-linux-amd64.sh
@@ -1,4 +1,4 @@
-sudo apt-get install python3-venv  # Untuk Ubuntu/Debian
+sudo apt-get install python3-venv libtiff5 libxcb-cursor0
 python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
